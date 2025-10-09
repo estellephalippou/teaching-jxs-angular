1acb0562b30433424e8b6450c9465f1c6b0ab194
iikraamm <i.rehahla@esi-sba.dz>
2025-10-09 08:14:19 +0200

    web tp angular : the rest of the  questions

--- pokedemo/src/app/app-module.ts
+++ pokedemo/src/app/app-module.ts
@@ -1,21 +1,24 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { MyComponent } from './my-component/my-component';
 
 import { FormsModule } from '@angular/forms';
+import { FilterPokemonPipePipe } from './filter-pokemon--pipe-pipe';
 
 @NgModule({
   declarations: [
     App,
-    MyComponent
+    MyComponent,
+    FilterPokemonPipePipe
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
